dfd058ef8c97045f7e038f3f21041967d50f90a7
Gabriela de Freitas <gabriela.freitas@dbccompany.com.br>
2022-11-12 18:52:23 -0300

    ajuste teste Black Friday

--- src/test/java/br/com/aula/pages/BannerPage.java
+++ src/test/java/br/com/aula/pages/BannerPage.java
@@ -8,12 +8,12 @@ public class BannerPage extends BasePage{
     private static final By textValidate =
             By.cssSelector("#root-app > div > div.ui-search-main.ui-search-main--deals.ui-search-main--without-header.ui-search-main--only-products.shops__search-main > aside > div.ui-search-breadcrumb.shops__breadcrumb > h1");
 
+    private static final By plantaoBlackFriday =
+            By.cssSelector("#root-app > div > div:nth-child(1) > section > div > div > div > div > div > div:nth-child(3) > a");
+
     public String validarTextNaTela(){
         return getText(textValidate);
     }
-
-    private static final By plantaoBlackFriday =
-            By.cssSelector("#root-app > div > div:nth-child(1) > section > div > div > div > div > div > div:nth-child(3) > a");
 
     public void clicarPlantaoBlackFriday(){
         click(plantaoBlackFriday);
